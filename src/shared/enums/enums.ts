2779c6b87ffb6d0897078e66a6339724a5501974
mopore <jni@mopore.org>
2023-12-23 15:30:44 +0100

    Update on shared types and classes.

---
+++ src/shared/enums/enums.ts
@@ -0,0 +1,50 @@
+export const enums = {
+	/**
+	 * This function is used to convert a string to an enum value.
+	 * It will throw an error if the conversion fails. 
+	 * @param enumObj The enum type with a string to convert to
+	 * @param value The string to convert to enum
+	 * @returns The enum value
+	 */
+	to: <E extends { [s: string]: string }>(
+		enumObj: E, 
+		value: string
+	): E[keyof E] => {
+		const enumValues = Object.values(enumObj);
+		if (enumValues.includes(value as unknown as E[keyof E])) {
+			return value as unknown as E[keyof E];
+		}
+		const errMsg = `Could not convert "${value}" to enum value`;
+		console.error(errMsg);
+		console.trace();
+		throw new Error(errMsg);
+	},
+
+	/**
+	 * This function is used to convert a string to an enum value.
+	 * It will throw an error if the conversion fails. 
+	 * @param enumObjA The A enum type with string values to convert to
+	 * @param enumObjB The B enum type with string values to convert to
+	 * @param value The string to convert to enum
+	 * @returns The enum value
+	 */
+	toAB: <A extends { [s: string]: string }, B extends { [s: string]: string }>(
+		enumObjA: A,
+		enumObjB: B,
+		value: string
+	): A[keyof A] | B[keyof B] => {
+		const valuesA = Object.values(enumObjA);
+		const valuesB = Object.values(enumObjB);
+
+		if (valuesA.includes(value as unknown as A[keyof A])) {
+			return value as unknown as A[keyof A];
+		} else if (valuesB.includes(value as unknown as B[keyof B])) {
+			return value as unknown as B[keyof B];
+		}
+
+		const errMsg = `Could not convert "${value}" to enum value`;
+		console.error(errMsg);
+		console.trace();
+		throw new Error(errMsg);
+	},
+}
