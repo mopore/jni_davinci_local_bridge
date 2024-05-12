62b30c0b06756d440859febd317727220b76ae25
mopore <jni@mopore.org>
2024-05-12 23:44:45 +0200

    Update on shared types and classes.

--- src/shared/SharedTopics.ts
+++ src/shared/SharedTopics.ts
@@ -4,4 +4,6 @@ export const sharedTopics = {
 	HALO_SYSTEM_COMMAND: "jniHome/services/halo/command" as const,
 	HALO_USER_COMMAND: "jniHome/services/halo/commandUser" as const,
 	HALO_MOVE_HISTORY: "jniHome/services/halo/moveHistory" as const,
+	PAN_CHECK_RESULTS: "pan/check" as const,
+	PAN_UPCOMING_EVENTS: "pan/upcomingEvents" as const,
 }
